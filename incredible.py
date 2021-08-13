840401bf87962b9cabb154a671957ac344e681c9
shivam-op <84925754+shivam-op@users.noreply.github.com>
2021-08-13 08:53:34 +0530

    Update incredible.py

--- incredible.py
+++ incredible.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import sys
 import random
@@ -9,7 +10,7 @@ from telethon.tl.functions.account import UpdateProfileRequest
 from Config import STRING, SUDO, BIO_MESSAGE, API_ID, API_HASH, STRING2, STRING3, STRING4 ,STRING5, STRING6, STRING7, STRING8 ,STRING9, STRING10, STRING11, STRING12 , STRING13 , STRING14 , STRING15 ,STRING16 , STRING17 , STRING18 , STRING19 , STRING20 , STRING21 , STRING22 , STRING23 , STRING24 , STRING25 
 import asyncio
 import telethon.utils
-from telethon.tl import functions
+from telethon.tl import functions,types
 from telethon.tl.functions.channels import LeaveChannelRequest
 from telethon.tl.functions.messages import ImportChatInviteRequest
 from Utils import RAID, RRAID
